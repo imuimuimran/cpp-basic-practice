389f8c3ae6f98a562f9a8523f2b304b4254604eb
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-05-22 13:48:34 +0600

    example of bubble sort in CPP

---
+++ bubble_sort_02.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity = O((n-1)*2)/2 = O(n^2) --> This Time Complexity is more efficient than O(n^2)
+// Memory Complexity = O(N)
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int>arr(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int pass=0; pass<n; pass++)
+    {
+        int last = n-1-pass;
+        bool sorted = true;
+        for(int j=0; j<=last-1; j++)
+        {
+            if(arr[j] > arr[j+1])
+                swap(arr[j],arr[j+1]);
+                sorted = false;
+        }
+        if(sorted)
+        {
+            break;
+        }
+        cout << "After pass: " << pass << ": ";
+        for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    }
+    cout << "After sort: " << "\n";
+    for(int i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
+
+/*
+7
+3 5 1 4 2 6 1
+*/
