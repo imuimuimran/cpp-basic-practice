055aca204f29763dfd7ec826689c7a0c70805719
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-05-28 13:38:56 +0600

    This C++ program can sort any array with deque algorithm

---
+++ deque_cocktail_sort_01.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+// Time Complexity: O(n^2)
+void cocktailSort(deque<int>& arr) {
+    bool swapped = true;
+    int start = 0;
+    int end = arr.size() - 1;
+
+    while (swapped) {
+        swapped = false;
+
+        // Move the largest element to the right
+        for (int i = start; i < end; i++) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+
+        swapped = false;
+
+        // Move the smallest element to the left
+        end--;
+        for (int i = end - 1; i >= start; i--) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        start++;
+    }
+}
+
+int main() {
+    deque<int> arr;
+    int n, x;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    cocktailSort(arr);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
